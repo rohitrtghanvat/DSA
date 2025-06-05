f95a9bab19c92ea0ee61caa931eb8ef4fa473bfd
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-06-05 20:59:19 +0530

    Updated main.java and deleted main.class

--- main.java
+++ main.java
@@ -1,5 +1,57 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Hello from A:\\DSA!");
-    }
-}
+import java.util.*;
+
+// public class Main{
+//     public static void main(String args[]){
+//         for(int counter =0 ;counter < 12 ; counter++){
+//             System.out.println("Hello World");
+//         }
+//     }
+// }
+
+// public class Main{
+//     public static void main(String args[]){
+//         for(int counter = 0 ; counter <11;counter ++){
+//             System.out.println(counter+"");
+//         }
+//     }
+// }
+
+// public class Main{
+//     public static void main(String  args[]){
+//         for(int i=0 ; i <12 ; i++){
+//             System.out.println(i);
+//         }
+//     }}
+
+//  public class Main{
+//     public static void main(String arg[]){
+//     int i=0;
+    
+//     while(i<11){
+//          System.out.println(i++);
+//     }
+//     }}
+
+// public class Main{
+//      public static void main(String arg[]){
+//           int i=0;
+//           do{
+//                System.out.println(i);
+//                i= i+1;
+//           }while(i < 11);
+//      }
+// }
+
+// public class Main{
+//      public static void main(String arg[]){
+//           Scanner sc = new Scanner(System.in);
+//           System.out.println("Please enter a number to generate its table:");
+//           int n = sc.nextInt();
+
+          
+//           for (int i=1; i<=10; i++){
+//                System.out.println(i*n);
+//           }
+          
+//      }
+// }
