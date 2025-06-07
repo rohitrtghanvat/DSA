8cda846642bfb7c44add37b3599d7f63f3a8db6d
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-06-07 22:41:37 +0530

    Updated Pattern.java and removed temp.java

--- Pattern.java
+++ Pattern.java
@@ -112,30 +112,31 @@
 
 // ----------------------------------------------------------
 // Inverted Half Pyramid (rotate by 180 degree)
-//      *
-//     **
-//    ***
-//   ****
-//  *****
+// *
+// **
+// ***
+// ****
+// *****
 // ******
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Pattern {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+// public class Pattern {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter value to print Inverted Half Pyramid (rotate by 180 degree) :- ");
-        int n = sc.nextInt();
+// System.out.print("Enter value to print Inverted Half Pyramid (rotate by 180
+// degree) :- ");
+// int n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-}
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= n - i; j++) {
+// System.out.print(" ");
+// }
+// for (int j = 1; j <= i; j++) {
+// System.out.print("*");
+// }
+// System.out.println();
+// }
+// }
+// }
