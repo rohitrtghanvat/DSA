b98bf0490868c6ea1c0c32cb732cbe0bac56593f
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-06-08 22:11:31 +0530

    update new patterns

--- Pattern.java
+++ Pattern.java
@@ -112,10 +112,10 @@
 
 // ----------------------------------------------------------
 // Inverted Half Pyramid (rotate by 180 degree)
-//     *
-//    **
-//   ***
-//  ****
+// *
+// **
+// ***
+// ****
 // *****
 
 // import java.util.Scanner;
@@ -124,7 +124,8 @@
 // public static void main(String args[]) {
 // Scanner sc = new Scanner(System.in);
 
-// System.out.print("Enter value to print Inverted Half Pyramid (rotate by 180degree) :- ");
+// System.out.print("Enter value to print Inverted Half Pyramid (rotate by
+// 180degree) :- ");
 // int n = sc.nextInt();
 
 // for (int i = 1; i <= n; i++) {
@@ -140,5 +141,114 @@
 // }
 
 // ----------------------------------------------------------
+// 6. print Half Pyramid with Numbers
+// Enter value for print Half Pyramid with Numbers :- 6
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+// 1 2 3 4 5 6
+// import java.util.Scanner;
 
+// public class Pattern {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in);
 
+// System.out.print("Enter value for print Half Pyramid with Numbers :- ");
+// int n = sc.nextInt();
+
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= i; j++) {
+// System.out.print(j + " ");
+// }
+// System.out.println();
+// }
+// sc.close();
+// }
+// }
+
+// ----------------------------------------------------------
+// Enter value to print Inverted Half With Numbers :- 5
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+// import java.util.Scanner;
+
+// public class Pattern {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in);
+
+// System.out.print("Enter value to print Inverted Half With Numbers :- ");
+// int n = sc.nextInt();
+
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= n - i + 1; j++) {
+// System.out.print(j + " ");
+// }
+// System.out.println();
+// }
+// sc.close();
+// }
+// }
+
+// ----------------------------------------------------------
+// 8.Floyd's Triangle
+// Enter value to print Floyd's Triangle :- 5
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+// import java.util.Scanner;
+
+// public class Pattern {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in);
+
+// System.out.print("Enter value to print Floyd's Triangle :- ");
+// int n = sc.nextInt();
+// int number = 1;
+
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= i; j++) {
+// System.out.print(number + " ");
+// number++; // number = number + 1
+// }
+// System.out.println();
+// }
+// }
+// }
+
+// ----------------------------------------------------------
+// 9. 0-1 Triangle
+// Enter value to print 0-1 Triangle :-5
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+// import java.util.Scanner;
+
+// public class Pattern {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in);
+
+// System.out.print("Enter value to print 0-1 Triangle :-");
+// int n = sc.nextInt();
+
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= i; j++) {
+// int sum = i + j;
+// if (sum % 2 == 0) {
+// System.out.print("1 ");
+// } else {
+// System.out.print("0 ");
+// }
+// }
+// System.out.println();
+// }
+// }
+// }
