a48ce5296fc844e2175889cf9c1e6e9692afc0df
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-06-19 21:20:34 +0530

    Print inverted number pyramid with decreasing rows

---
+++ Pattern/InvertedHalfNumber.java
@@ -0,0 +1,26 @@
+// Enter value to print Inverted Half With Numbers :- 5
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+package Pattern;
+
+import java.util.Scanner;
+
+public class InvertedHalfNumber {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value to print Inverted Half With Numbers :- ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
