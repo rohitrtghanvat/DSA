8fed906145f9623b51670ef66c52e1f517bb6cc6
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-06-19 20:45:22 +0530

    Implement Hollow Rectangle pattern using nested loop with user input

---
+++ Pattern/HollowRectangle.java
@@ -0,0 +1,33 @@
+// Enter how many row you want :-5
+// Enter how many column yoi want :-5
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+package Pattern;
+
+import java.util.Scanner;
+
+public class HollowRectangle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter how many row you want :-");
+        int n = sc.nextInt();
+
+        System.out.print("Enter how many column yoi want :-");
+        int m = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || i == n || j == 1 || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
