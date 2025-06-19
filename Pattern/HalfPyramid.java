f06f73ddb03f47d91ecbcd2d225f7d280ac9e9eb
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-06-19 20:53:38 +0530

    Added Half Pyramid star pattern logic (row-wise incremental stars)

---
+++ Pattern/HalfPyramid.java
@@ -0,0 +1,26 @@
+// 3. Half Pyramid
+// Enter value to print Half Pyramid format :-5
+// *
+// **
+// ***
+// ****
+// *****
+package Pattern;
+
+import java.util.Scanner;
+
+public class HalfPyramid {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value to print Half Pyramid format :-");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
