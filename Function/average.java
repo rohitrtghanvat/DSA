87bcdecaa965d39db67de7c3358b4eaa121d1ce5
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-06-21 23:19:28 +0530

    Add Average.java to compute average of three input numbers correctly

---
+++ Function/average.java
@@ -0,0 +1,22 @@
+// Enter 3 numbers from the user & make a function to print their average.
+
+package Function;
+
+import java.util.Scanner;
+
+public class average {
+    public static float printaverage(float a, float b, float c) {
+        return (a * b * c) / 3;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three number to print their average :-");
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
+
+        System.out.print("average of three number is :-  " + printaverage(a, b, c));
+        sc.close();
+    }
+}
