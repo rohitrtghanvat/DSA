891e43c47db4d3629efcc0ca38fdff305572e610
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-07-01 23:48:53 +0530

    feat: Demonstrate String representation using literal and new keyword

---
+++ String/representString.java
@@ -0,0 +1,17 @@
+package String;
+
+public class representString {
+    public static void main(String[] args) {
+        // Declare String without using new operator
+        String s = "I love Cricket";
+
+        // Prints the String.
+        System.out.println("String s = " + s);
+
+        // Declare String using new operator
+        String s1 = new String("I love Cricket");
+
+        // Prints the String.
+        System.out.println("String s1 = " + s1);
+    }
+}
