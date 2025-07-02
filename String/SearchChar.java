db991d3d0177b4842b1e579ce10b2c46b3022270
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-07-02 23:42:40 +0530

    ✨ Added SearchChar to find first occurrence of a character in a string

---
+++ String/SearchChar.java
@@ -0,0 +1,29 @@
+package String;
+
+public class SearchChar {
+    // function to find the first occurrence of ch in s
+    static int findChar(String s, char ch) {
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            // If the current character is equal to ch,
+            // return the current index
+            if (s.charAt(i) == ch) {
+                return i;
+            }
+        }
+        // If we did not find any occurrence of ch,
+        // return -1
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        String s = "Search using index match";
+        // 0123456789
+        char ch = 'i';
+
+        System.out.print(" index :- " + findChar(s, ch));
+    }
+}
+
+// output
+//  index :- 9
